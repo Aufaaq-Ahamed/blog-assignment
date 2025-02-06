9657c322ef4be3ff20b84caeeb2604b87c010509
Aufaaq Ahamed <aufaaq.a@jmangroup.com>
2025-02-06 12:39:44 +0530

    final

---
+++ src/components/Blog.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+
+export function Blog({ blogs, setBlogs }) {
+    const navigate = useNavigate();
+
+    function deleteBlog(id) {
+        const updatedBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(updatedBlogs);
+        localStorage.setItem("blogs", JSON.stringify(updatedBlogs));
+    }
+
+    function addViews(id) {
+        const updatedBlogs = blogs.map(blog => blog.id === id ? { ...blog, views: blog.views + 1 } : blog);
+        setBlogs(updatedBlogs);
+        localStorage.setItem("blogs", JSON.stringify(updatedBlogs));
+    }
+
+    return (
+        <div className="min-h-screen flex font-mono flex-wrap justify-center m-6 px-6 py-12 gap-8">
+            {blogs.map((blog) => (
+                <div key={blog.id} className="bg-white w-80 h-90 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out">
+                    <Link onClick={() => addViews(blog.id)} to={`/content/${blog.id}`}>
+                        <img src={blog.image} alt="Blog" className="w-full h-56 object-cover rounded-t-lg" />
+                    </Link>
+                    <div className="p-4">
+                        <div className="font-semibold text-2xl text-gray-800 truncate">{blog.title}</div>
+                        <div className="flex justify-between items-center mt-4">
+                            <button
+                                onClick={() => deleteBlog(blog.id)}
+                                className="px-4 py-2 text-white bg-red-600 rounded-full text-lg hover:bg-red-700 transition duration-300">
+                                Delete
+                            </button>
+                            <Link to={`/create?edit=${blog.id}`}>
+                                <button className="px-4 py-2 text-white bg-green-600 rounded-full text-lg hover:bg-green-700 transition duration-300">
+                                    Edit
+                                </button>
+                            </Link>
+                            <p className="text-gray-600 font-medium text-xl">👁️{blog.views}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
---
+++ src/components/Create.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export function Create({ setBlogs }) {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+
+    const [newItem, setNewItem] = useState({ id: crypto.randomUUID(), title: "", content: "", image: null, views: 0 });
+    const editId = searchParams.get("edit");
+    useEffect(() => {
+        const editId = searchParams.get("edit");
+        if (editId) {
+            const savedBlogs = JSON.parse(localStorage.getItem("blogs") || "[]");
+            const blogToEdit = savedBlogs.find(blog => blog.id === editId);
+            if (blogToEdit) {
+                setNewItem(blogToEdit);
+            }
+        }
+    }, [searchParams]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const updatedBlogs = JSON.parse(localStorage.getItem("blogs") || "[]");
+
+        if (newItem.id === editId) {
+            const updatedBlogsArray = updatedBlogs.map(blog =>
+                blog.id === newItem.id ? newItem : blog
+            );
+            console.log("hello")
+            setBlogs(updatedBlogsArray);
+            localStorage.setItem("blogs", JSON.stringify(updatedBlogsArray));
+        } else {
+            setBlogs(prevBlogs => [...prevBlogs, newItem]);
+            localStorage.setItem("blogs", JSON.stringify([...updatedBlogs, newItem]));
+        }
+        navigate("/blog");
+    }
+
+    function handleFileChange(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setNewItem(prev => ({ ...prev, image: reader.result }));
+        };
+    }
+
+    return (
+        <div id="postFormContainer" className="bg-white p-6 h-screen rounded-lg shadow-md bg-opacity-75">
+            <h2 className="text-2xl font-bold mb-4">
+                {searchParams.get("edit") ? "Edit Post" : "Create a New Post"}
+            </h2>
+            <form id="postForm" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={newItem.title}
+                    onChange={(e) => setNewItem({ ...newItem, title: e.target.value })}
+                    placeholder="Title"
+                    className="w-full p-2 border rounded mb-4 text-3xl hover:ring-2 ring-blue-500"
+                    required
+                />
+                <textarea
+                    value={newItem.content}
+                    onChange={(e) => setNewItem({ ...newItem, content: e.target.value })}
+                    placeholder="Content"
+                    className="w-full p-2 border rounded mb-4 h-32 text-xl hover:ring-2 ring-blue-500"
+                    required
+                ></textarea>
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    className="w-full p-2 border rounded mb-4 text-3xl hover:ring-2 ring-blue-500"
+                    required
+                />
+                <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 ">
+                    {searchParams.get("edit") ? "Update Post" : "Save Post"}
+                </button>
+            </form>
+        </div>
+    );
+}
